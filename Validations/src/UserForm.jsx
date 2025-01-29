4912bd5dcfcf8b03db0685f64fc179b2d6d6e3f2
dhanjiRajput <kidechadhanji@gmail.com>
2025-01-29 23:04:06 +0530

    updated

--- Validations/src/UserForm.jsx
+++ Validations/src/UserForm.jsx
@@ -16,28 +16,42 @@ const validations=z.object({
 });
 
 const UserForm = () => {
-
-    const {register,handleSubmit,formState:{errors}}=useForm({
-        resolver:zodResolver(validations)
+    const {register,watch,handleSubmit,formState:{errors}}=useForm({
+        resolver:zodResolver(validations),
+        mode:"onChange",
     });
 
     const onSubmit=(data)=>{
         console.log(data);
     };
+    watch();
+
+    const value=watch();
+
+   const box=(name)=>{
+        if(errors[name]){
+            return "2px solid red"
+        }else if(value && !errors[name]){
+            return "2px solid green"
+        }else{
+            return "2px solid black"
+        }
+   };
+
     return(
         <>
             <div>
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <label htmlFor='name'>Enter User Name</label>
-                    <input type='text' id='name' {...register("name")}></input>
+                    <input type='text' id='name' style={{border:box("name")}} {...register("name")}></input>
                     {errors.name && <span>{errors.name.message}</span>}
                     <br/><br/>
                     <label htmlFor='email'>Enter User Email</label>
-                    <input type='email' id='email' {...register("email")} ></input>
+                    <input type='email' id='email' style={{border:box("email")}} {...register("email")} ></input>
                     {errors.email && <span>{errors.email.message}</span>}
                     <br/><br/>
                     <label htmlFor='password'>Enter User Password</label>
-                    <input type='password' id='password' {...register("password")}></input>
+                    <input type='password' id='password' style={{border:box("password")}} {...register("password")}></input>
                     {errors.password && <span>{errors.password.message}</span>}
                     <br/><br/>
                     <button type='submi'>Submit</button>
